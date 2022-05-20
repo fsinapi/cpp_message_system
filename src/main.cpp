6a752359ebc8d94011655cab2f5b68374ed23d1b
Frank Sinapi <frank.sinapi@opentrons.com>
2022-05-20 11:49:00 -0400

    Updates to concept definition to allow better type checking for Tasklist

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,11 @@ auto main() -> int {
     try {
         tasklist.register_queue<TaskIndex::Task2>(queue_2);
     } catch(std::bad_exception &e) {
-        std::cout << "exception: " << e.what() << std::endl;
+        std::cout << "Tried to set same queue twice: " << e.what() << std::endl;
     }
 
     std::cout << "Queue1 index: " 
-        << static_cast<size_t>(TaskIndex::Task1) 
+        << TaskIndex::Task1
         << std::endl;
     std::cout << "Queue2 index: " 
         << MyTasks::get_task_idx<Task2Message>() 
@@ -36,10 +36,35 @@ auto main() -> int {
     // Send message to Queue 1
     tasklist.send_to<TaskIndex::Task1>(Message1{.id=505});
 
-    auto rcv = queue_1.try_read();
-    std::cout << "Received: " << rcv.has_value() << std::endl;
+    auto rcv1 = queue_1.try_read();
+    std::cout << "Received: " << rcv1.has_value() << std::endl;
+    {
+        auto msg = rcv1.value();
+        std::cout << "Message 1 idx: " << msg.index() << std::endl;
+    }
 
     tasklist.send_to<TaskIndex::Task2>(Message3{.id=0, .payload=4});
-    
+    auto rcv2 = queue_2.try_read();
+    {
+        auto msg = rcv2.value();
+        std::cout << "Message 2 idx: " << msg.index() << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Sending message with deduced mailbox" << std::endl;
+    tasklist.send(Message1{.id=999});
+    auto rcv3 = queue_1.try_read();
+    {
+        std::cout << "Received: " << rcv3.has_value() << std::endl;
+        auto msg = rcv3.value();
+        std::cout << "Message 3 idx: " << msg.index() << std::endl;
+        auto val = std::get<Message1>(msg).id;
+        std::cout << "Message 3 value: " << val << std::endl;
+    }
+
+    // If a message is in multiple messsage definitions, must
+    // disambiguate with a variant constructor
+    tasklist.send(Task2Message(Message2{.id=1, .payload=2}));
+
     return 0;
 }
